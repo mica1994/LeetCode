ef1a644410c0f44f5fc36da677f88393b949a38d
mica1994 <948933184@qq.com>
2019-12-25 11:22:20 +0800

    better algorithm （maybe）

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,5 @@
-// 暴力法，两层循环时间复杂度O(n^2)，空间复杂度O(1)
-public class TwoSum {
+// 暴力法，两层循环，时间复杂度O(n^2)，空间复杂度O(1)
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j < nums.length; j++) {
@@ -7,6 +7,21 @@ public class TwoSum {
                     return new int[]{i, j};
                 }
             }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+// 一遍哈希法，在进行迭代并将元素插入到表中的同时，检查表中是否已经存在当前元素所对应的目标元素，时间复杂度O(n)，空间复杂度O(n)
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
