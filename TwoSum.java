ed4ff8de4b0c516b5da2549e595c5f0b4291a315
mica1994 <948933184@qq.com>
2019-12-25 11:16:28 +0800

    LeetCode-1-两数之和
    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。

---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+// 暴力法，两层循环时间复杂度O(n^2)，空间复杂度O(1)
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
